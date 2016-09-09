b6cc71660b3f7149bce2ada9b4f22f49dc1214a2
jordvnkm <minatogawaj@gmail.com>
2016-09-08 20:49:00 -0700

    started account page

--- frontend/components/account_page.jsx
+++ frontend/components/account_page.jsx
@@ -1,15 +1,37 @@
 const React = require("react");
 const NavBar = require("./navbar");
-
+const hashHistory = require("react-router").hashHistory;
 
 const AccountPage = React.createClass({
   getInitialState: function(){
-
+    return {publishedPosts: [], unpublishedPosts: []}
   },
 
   componentWillMount: function(){
-
+    if (window.FB == undefined){
+      this.loadFBSDK();
+    }
+    else {
+      this.checkLoginState();
+    }
   },
+
+  statusChangeCallback: function(response){
+    if (response.status == "success"){
+      
+    }
+    else {
+      console.log("not connected from account page");
+
+    }
+  },
+
+  checkLoginState: function(){
+      FB.checkLoginState(function(response){
+        this.statusChangeCallback(response);
+      }.bind(this));
+  },
+
   render: function(){
     return(
       <div className="accountPage">
