ff7e6a9f24061558a22f4795f5b893c9e52c9c42
jordvnkm <minatogawaj@gmail.com>
2016-09-14 17:53:08 -0700

    changed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root "static_pages#index"
 
+  get '/facebook', to: "facebooks#index"
   namespace :api, defaults: {format: :json} do
 
-    resource :facebook, only: [:index, :create]
-    resources :users, only: [:index, :show, :create, :update]
-    resource :session, only: [:show, :create, :destroy]
+    # resource :facebook, only: [:index, :create]
+    # resources :users, only: [:index, :show, :create, :update]
+    # resource :session, only: [:show, :create, :destroy]
 
   end
 end
